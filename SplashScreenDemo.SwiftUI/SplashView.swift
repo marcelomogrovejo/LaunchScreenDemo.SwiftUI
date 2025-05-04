85d6471644b2fc54d98891d76a57a01a127abbd3
Marcelo Mogrovejo <marcelomogrovejo@gmail.com>
2025-05-04 15:43:29 +0800

    first commit

---
+++ SplashScreenDemo.SwiftUI/SplashView.swift
@@ -0,0 +1,18 @@
+//
+//  SplashView.swift
+//  SplashScreenDemo.SwiftUI
+//
+//  Created by Marcelo Mogrovejo on 04/05/2025.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashView()
+}
