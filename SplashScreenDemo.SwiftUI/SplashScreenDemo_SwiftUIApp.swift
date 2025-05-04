e4aee96e6824e19e97f9f1c5b04e9da7ff4939d0
Marcelo Mogrovejo <marcelomogrovejo@gmail.com>
2025-05-04 13:12:03 +0800

    Initial Commit

---
+++ SplashScreenDemo.SwiftUI/SplashScreenDemo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashScreenDemo_SwiftUIApp.swift
+//  SplashScreenDemo.SwiftUI
+//
+//  Created by Marcelo Mogrovejo on 04/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SplashScreenDemo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
